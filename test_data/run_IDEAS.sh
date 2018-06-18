bd2c10f96c2e1f3d24fa352d34ade43b4910fb78
Universe <universe@MacBook-Air.local>
2018-06-18 01:57:17 -0400

    add testing data

--- test_data/run_IDEAS.sh
+++ test_data/run_IDEAS.sh
@@ -1,5 +1,5 @@
 ###### run IDEAS
 mkdir run_IDEAS_result
-cp -r /Users/universe/Documents/2018_BG/IDEAS_2018/bin ./
+cp -r ~/group/software/IDEAS/IDEAS_2018/test_data/IDEAS_2018/bin ./
 time Rscript bin/runme.R run_IDEAS.input run_IDEAS.parafile run_IDEAS_result
 
