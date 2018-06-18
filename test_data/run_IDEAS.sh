a1c533431a68410ece3a9c63fb31a6810389df72
guanjue <guanjuexiang@gmail.com>
2018-06-18 13:58:57 -0400

    add readme file

--- test_data/run_IDEAS.sh
+++ test_data/run_IDEAS.sh
@@ -4,11 +4,11 @@
 IDEAS_job_name=test_IDEAS
 output_folder=/storage/home/gzx103/group/software/IDEAS/IDEAS_2018/test_data/run_IDEAS_result/
 
+### make output folder
 mkdir -p $output_folder
+### cp scripts to the analysis folder
 cp -r ~/group/software/IDEAS/IDEAS_2018/bin ./
-### run IDEAStime Rscript bin/runme.R run_IDEAS.input run_IDEAS.parafile $output_folder
-### get GenomeBrowser Tracks
-rm $output_folder*tmp*
-find `pwd` -name *chr*.state > state_file_list.txttime Rscript bin/runme.R run_IDEAS.input run_IDEAS_get_tracks.parafile $output_folder
+### run IDEAS
+time Rscript bin/runme.R run_IDEAS.input run_IDEAS.parafile $output_folder
 ### get heatmap
 time Rscript bin/get_heatmap.R $output_folder$IDEAS_job_name'.para0' FALSE ~/group/software/IDEAS/IDEAS_2018/bin/createGenomeTracks.R
