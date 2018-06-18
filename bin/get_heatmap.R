bffa16895f9c103640d2eb7ea182535028107ef3
Universe <universe@MacBook-Air.local>
2018-06-18 02:34:13 -0400

    add script

---
+++ bin/get_heatmap.R
@@ -0,0 +1,10 @@
+args = commandArgs(trailingOnly=TRUE)
+input_parafile = args[1]
+scale_use = as.logical(args[2])
+source_file = args[3]
+
+source(source_file)
+
+pdf(paste(input_parafile, '.pdf', sep=''))
+creatHeatmap(input_parafile, scale=scale_use)
+dev.off()
--- test_data/run_IDEAS.sh
+++ test_data/run_IDEAS.sh
@@ -1,5 +1,11 @@
 ###### run IDEAS
-mkdir run_IDEAS_result
-cp -r ~/group/software/IDEAS/IDEAS_2018/test_data/IDEAS_2018/bin ./
-time Rscript bin/runme.R run_IDEAS.input run_IDEAS.parafile run_IDEAS_result
-
+######
+### cp script in the folder
+mkdir -p run_IDEAS_result
+cp -r ~/group/software/IDEAS/IDEAS_2018/bin ./
+### run IDEAS
+time Rscript bin/runme.R /storage/home/gzx103/group/software/IDEAS/IDEAS_2018/test_data/run_IDEAS.input /storage/home/gzx103/group/software/IDEAS/IDEAS_2018/test_data/run_IDEAS.parafile /storage/home/gzx103/group/software/IDEAS/IDEAS_2018/test_data/run_IDEAS_result
+### get GenomeBrowser Tracks
+time Rscript bin/runme.R /storage/home/gzx103/group/software/IDEAS/IDEAS_2018/test_data/run_IDEAS.input /storage/home/gzx103/group/software/IDEAS/IDEAS_2018/test_data/run_IDEAS_get_tracks.parafile /storage/home/gzx103/group/software/IDEAS/IDEAS_2018/test_data/run_IDEAS_result
+### get heatmap
+time Rscript bin/get_heatmap.R run_IDEAS_result/test_IDEAS.para0 FALSE ~/group/software/IDEAS/IDEAS_2018/bin/createGenomeTracks.R
