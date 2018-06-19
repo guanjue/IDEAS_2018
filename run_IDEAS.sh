ff905457792abef20c0be75026fbab38375ab375
Universe <universe@MacBook-Air.local>
2018-06-19 00:12:00 -0400

    IDEAS_2018 DONE

--- run_IDEAS.sh
+++ run_IDEAS.sh
@@ -7,15 +7,15 @@ output_folder=/storage/home/gzx103/group/software/IDEAS/IDEAS_2018/test_data/run
 binfile=mm10_noblacklist_200bin.bed
 
 ### make output folder
-mkdir -p $output_folder
+if [ -d $output_folder ]; then rm -r $output_folder; mkdir $output_folder; else mkdir $output_folder; fi
 ### cp scripts to the analysis folder
 if [ -d bin ]; then rm -r bin; cp -r $script_folder'bin' ./; else cp -r $script_folder'bin' ./; fi
 if [ -d data ]; then rm -r bin; cp -r $script_folder'data' ./; else cp -r $script_folder'data' ./; fi
 ### get genome inv file
-time python $script_folder'bin/bed2inv.py' -i $binfile -o $binfile'.inv'
+time python ./bin/bed2inv.py -i $binfile -o $binfile'.inv'
 ### run IDEAS
-time Rscript bin/runme.R $IDEAS_job_name'.input' $IDEAS_job_name'.parafile' $output_folder
+time Rscript ./bin/runme.R $IDEAS_job_name'.input' $IDEAS_job_name'.parafile' $output_folder
 ### rm tmp files
 rm $output_folder*tmp*
 ### get heatmap
-time Rscript bin/get_heatmap.R $output_folder$IDEAS_job_name'.para0' FALSE ~/group/software/IDEAS/IDEAS_2018/bin/createGenomeTracks.R
+time Rscript bin/get_heatmap.R $output_folder$IDEAS_job_name'.para0' FALSE ./bin/createGenomeTracks.R
